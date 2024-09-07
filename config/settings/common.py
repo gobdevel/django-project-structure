cc895d6e9e92f998a70b972b648a79abf366d503
Gobind Prasad <gobdeveloper@gmail.com>
2024-09-07 00:16:09 +0000

    Add custom user model

--- config/settings/common.py
+++ config/settings/common.py
@@ -31,7 +31,8 @@ INSTALLED_APPS = [
     "corsheaders",
     "drf_standardized_errors",
     "rest_registration",
-    # local Apps
+    # local AppsA
+    "apps.users",
 ]
 
 MIDDLEWARE = [
@@ -146,6 +147,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.CustomUser"
 
 
 # Rest registration app
